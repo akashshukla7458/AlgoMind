eaae267392bc2d7f5934a9f7ecd830a8c6884a8b
[Akash Shukla] <[akashshukla7458@gmail.com]>
2023-03-03 23:07:11 +0530

    03mar

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import QuestionData from '../services/QuestionData';
+import './Questions.css'
 
 const QuestionsTable = ({ topicName }) => {
-  const questions = QuestionData.filter(q => q.topicName === topicName);
-   console.log('questions',questions)
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    const filteredQuestions = QuestionData.filter(q => q.topicName === topicName);
+    setQuestions(filteredQuestions);
+    // console.log(filteredQuestions);
+  }, [topicName]);
   
   return (
+    <div>
     <table>
       <thead>
         <tr>
@@ -19,19 +25,23 @@ const QuestionsTable = ({ topicName }) => {
         </tr>
       </thead>
       <tbody>
-        {questions.map((q, index) => (
-          <tr key={index}>
-            <td>{index + 1}</td>
-            <td>{q.Problem}</td>
-            <td>{q.Done !== undefined ? q.Done.toString() : ""}</td>
-            <td>{q.Bookmark !== undefined ? q.Bookmark.toString() : ""}</td>
-            <td>{q.Notes !== undefined ? q.Notes : ""}</td>
-            <td><a href={q.URL}>{q.URL !== undefined ? q.URL : ""}</a></td>
-            <td><a href={q.URL2}>{q.URL2 !== undefined ? q.URL2 : ""}</a></td>
-          </tr>
-        ))}
+      {questions.map((q, index) => (
+    q.questions.map((question, qIndex) => (
+      <tr key={qIndex}>
+        {/* <td>{index + 1}</td> */}
+        <td>{index * q.questions.length + qIndex + 1}</td>
+        <td>{question.Problem}</td>
+        <td>{question.Done !== undefined ? question.Done.toString() : ""}</td>
+        <td>{question.Bookmark !== undefined ? question.Bookmark.toString() : ""}</td>
+        <td>{question.Notes !== undefined ? question.Notes : ""}</td>
+        <td><a href={question.URL}>{question.URL !== undefined ? question.URL : ""}</a></td>
+        <td><a href={question.URL2}>{question.URL2 !== undefined ? question.URL2 : ""}</a></td>
+      </tr>
+    ))
+  ))}
       </tbody>
     </table>
+    </div>
   );
 };
 
